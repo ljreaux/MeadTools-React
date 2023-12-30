b2ae7f7b448e04d9dbd662c58475c70a6d32ea5b
Larry John Reaux <larryreaux@gmail.com>
2023-12-30 17:07:27 -0600

    finished topnav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,12 +33,52 @@ function Navbar() {
           >
             Nutrient Calculator
           </NavLink>
-          <NavLink
-            className="hover:text-background transition-colors sm:mx-4 mx-1"
-            to="/ExtraCalcs"
-          >
-            Extra Calculators
-          </NavLink>
+          <div className="flex flex-col group">
+            <NavLink
+              className="hover:text-background transition-colors sm:mx-4 mx-1 translate-y-44"
+              to="/ExtraCalcs"
+            >
+              Extra Calculators
+            </NavLink>
+            <div className="mt-48 h-fit w-full bg-sidebar grid rounded-xl opacity-0 group-hover:opacity-100 transition-all">
+              <Link to="/ExtraCalcs/" className="hover:text-background ">
+                ABV Calculator
+              </Link>{" "}
+              <Link
+                to="/ExtraCalcs/brixCalc"
+                className="hover:text-background "
+              >
+                Brix Conversion
+              </Link>
+              <Link to="/ExtraCalcs/estOG" className="hover:text-background ">
+                Estimated OG
+              </Link>
+              <Link to="/ExtraCalcs/sulfite" className="hover:text-background ">
+                Sulfite Addition
+              </Link>
+              <Link to="/ExtraCalcs/sorbate" className="hover:text-background ">
+                Sorbate Addition
+              </Link>
+              <Link
+                to="/ExtraCalcs/RefractometerCorrection"
+                className="px-2 hover:text-background "
+              >
+                Refractometer Correction
+              </Link>
+              <Link
+                to="/ExtraCalcs/tempCorrection"
+                className="hover:text-background "
+              >
+                Temperature Correction
+              </Link>
+              <Link
+                to="/ExtraCalcs/blending"
+                className="hover:text-background "
+              >
+                Blending
+              </Link>
+            </div>
+          </div>
           <NavLink
             className="hover:text-background transition-colors sm:mx-4 mx-1 pr-12"
             to="/documentation"
